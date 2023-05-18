c38dd6d44d4d446cf95cbd8eaf3687e35f37a15a
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-05-18 11:58:11 -0500

    prop entity map ready

--- re_gpt.py
+++ re_gpt.py
@@ -4,6 +4,8 @@ import json
 import argparse
 import gpt
 
+STOP_WORDS = ['the', 'of', 'a']
+
 def read_abstract(args):
     with open(args.abstract_file) as f:
         for line in f:
@@ -31,20 +33,37 @@ def main():
     args = get_args()
     for abstract in read_abstract(args):
         # Step 1, Get a start table draft with possibly complete property names
+        '''
         table_draft = get_table_draft(abstract)
         show_output(table_draft)
         with open('./output/table_draft.json', 'w') as f_o:
             f_o.write(json.dumps(table_draft))
-
+        
+        '''
         with open('./output/table_draft.json') as f:
             table_draft = json.load(f)
-
-        #Step 2, Get Entity by Property Name
-        res_entity = property_to_entity(abstract, table_draft)
-        print_msg(res_entity)
-        with open('./output/prop_entity.txt', 'w') as f_o:
-            f_o.write(res_entity)
         
+        #Step 2, Get Entity by Property Name
+        
+        '''
+        prop_entity_map = property_to_entity(abstract, table_draft)
+        print_msg(prop_entity_map)
+        with open('./output/prop_entity.json', 'w') as f_o:
+            f_o.write(json.dumps(prop_entity_map))
+        '''
+
+        #Sep 3, Check row by Property-Entity map
+        with open('./output/prop_entity.json') as f:
+            prop_entity_map = json.load(f)
+        
+        show_table(table_draft)
+        show_dict(prop_entity_map)
+
+def show_dict(dict_data):
+    print('_'*100)
+    for key in dict_data:
+        print(key, ' | ', '   '.join(dict_data[key]))
+
 def property_to_entity(abstract, row_data):
     prop_dict = {}
     for idx, row_item in enumerate(row_data):
@@ -55,7 +74,6 @@ def property_to_entity(abstract, row_data):
             prop_key = prop_name.lower()
             question = f'The {prop_name} of what entity is given explicitly?'
             prop_dict[prop_key] = question
-
     question_lst = []
     prop_lst = []
     for prop in prop_dict:
@@ -68,9 +86,26 @@ def property_to_entity(abstract, row_data):
         'questions':batch_question_text
     }
     entity_prompt = read_prompt('entity', field_dict) 
-    print_msg(entity_prompt)
     response = gpt.chat_complete(entity_prompt)
-    return response
+    answer_lst = response.split('\n')
+    assert (len(prop_lst) == len(answer_lst))
+    prop_entity_map = {}
+    sep = '#@'
+    for idx, prop_name in enumerate(prop_lst):
+        answer_text = answer_lst[idx]
+        assert(sep in answer_text)
+        prop_entity_text = answer_text.split(sep)[0]
+        prop_entity_lst = get_entity_from_answer(prop_name, prop_entity_text.lower())
+        prop_entity_map[prop_name] = prop_entity_lst
+    return prop_entity_map
+
+def get_entity_from_answer(prop_name, prop_entity_text):
+    prop_entity_text = prop_entity_text.replace(prop_name, '')
+    for stop_word in STOP_WORDS:
+        prop_entity_text = prop_entity_text.replace(stop_word, '')
+    prop_entity_text = prop_entity_text.strip()
+    entity_lst = prop_entity_text.split('|&')
+    return entity_lst
 
 def print_msg(msg):
     print('-'*100)
@@ -114,7 +149,7 @@ def get_table_draft(abstract):
     out_table_draft_sorted = sorted(out_table_draft, key=lambda x: x[1])
     return out_table_draft_sorted
 
-def show_output(output_row_data):
+def show_table(output_row_data):
     print('-'*100)
     for row_info in output_row_data:
         text = ' | '.join(row_info)
